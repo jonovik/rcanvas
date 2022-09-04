jonovik/rcanvas

commit 2755a3fb68895476aeff0d8c24c6bd5a42d46018

Fix pagination when link header has no rel=last entry.
Variable `pages` in function paginate() will become a vector of URLs.
The previous version wrongly initialized the first entry as the entire link header, rather than just the rel=first entry.

R/process_response.R
@@ -52,11 +52,11 @@ process_response <- function(url, args) {
 paginate <- function(x, showProgress=T) {
   first_response <- list(x)
   stopifnot(httr::status_code(x) == 200) # OK status
-  pages <- httr::headers(x)$link
+  pages <- get_page(x, "first")
   if (is.null(pages)) return(first_response)
   should_continue <- TRUE
   inc <- 2
-  if (has_rel(pages, "last")) {
+  if (has_rel(httr::headers(x)$link, "last")) {
     last_page <- get_page(x, "last")
     n_pages <- readr::parse_number(stringr::str_extract(last_page, "page=[0-9]{1,}"))
     if (n_pages == 1) return(first_response)

commit 7ef58599951ccc237ca0aec15c07d89f76e4ff0d

Have get_page() return NULL if no link header. Fixes behaviour of paginate().
Commit 2755a3f inadvertently broke paginate() for responses that do not require pagination.
This change to get_page() fixes it, seemingly with no ill effects.

R/process_response.R
@@ -101,6 +101,7 @@ has_rel <- function(x, rel) {
 
 get_page <- function(resp, page) {
   pages <- resp$headers$link
+  if (is.null(pages)) return(NULL)
   url_pattern <- "http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+"
   pages <- stringr::str_split(pages, ",")[[1]]
   url <- stringr::str_subset(pages, page)